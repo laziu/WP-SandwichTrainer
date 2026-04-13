be1f1ee938f6544e8974231dcb43ee1b1b88dda3
Geonu Kim <laziu.cc@gmail.com>
2026-04-13 18:36:28 +0900

    feat: input bindable component

---
+++ Game/Source/SandwichTrainer/Private/ASandwichPlayerController.cpp
@@ -0,0 +1,55 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ASandwichPlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "Interface/InputBindable.h"
+#include "UserExtension.h"
+
+ASandwichPlayerController::ASandwichPlayerController()
+{
+}
+
+void ASandwichPlayerController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+	InPawn->EnableInput(this);
+
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(InPawn->InputComponent))
+	{
+		for (UActorComponent* Comp : InPawn->GetComponents())
+		{
+			if (IInputBindable* Bindable = Cast<IInputBindable>(Comp))
+			{
+				Bindable->SetupInputBindings(EIC);
+			}
+		}
+	}
+}
+
+void ASandwichPlayerController::OnUnPossess()
+{
+	if (APawn* MyPawn = GetPawn())
+	{
+		if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(MyPawn->InputComponent))
+		{
+			EIC->ClearActionBindings();
+		}
+	}
+	Super::OnUnPossess();
+}
+
+void ASandwichPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (auto Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		if (InputMappingDefault)
+		{
+			Subsystem->AddMappingContext(InputMappingDefault, 0);
+		}
+	}
+}
